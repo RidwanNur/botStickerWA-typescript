5ebefa92aa6ea741c1534ea835c79ae6d54976de
ridwannhd0705 <ridwannur07@student.uns.ac.id>
2023-03-07 01:06:57 +0700

    iseng

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { Client, LocalAuth } from "whatsapp-web.js"; 
+import qrcode from 'qrcode-terminal';
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+});
+
+client.on('qr', (qr) => {
+        qrcode.generate(qr, {small : true});
+});
+
+client.on('ready', ()=> {
+    console.log('Client Ready');
+});
+
+client.on('message', async (message) => {
+    if(
+        (message.body.startsWith('sticker') || message.body.startsWith('/sticker')) && message.type === 'image'
+    )
+    {
+        const media = await message.downloadMedia();
+
+        client.sendMessage(message.from, media, {
+            sendMediaAsSticker: true,
+            stickerAuthor: '7kosong',
+            stickerName: 'Auah'
+        });
+    }
+});
+
+
+
+client.initialize();
